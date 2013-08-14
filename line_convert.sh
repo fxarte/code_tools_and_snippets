e00e7bb15642e17440745f9fc0a5867f29698b0a
felix a silberstein <fxarte@gmail.com>
2013-08-14 18:36:21 -0400

    Updated code for line_convert.sh

--- line_convert.sh
+++ line_convert.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
+function usage() {
+  echo -e "Invalid arguments.\n$0 <folder> [u2d | d2u]"
+}
 CURRENT_FOLDER="`pwd`"
 COMMANDS="d2u dos2unix;u2d unix2dos"
 I="u2d"
-COMMAND=(`echo $COMMANDS | tr ";" "\n" | grep $I`)
+if [ -n "$2" ]; then
+  I=$2
+fi
+COMMAND=(`echo $COMMANDS | tr ";" "\n" | grep $I 2>/dev/null`)
 COMMAND=${COMMAND[1]}
-echo $COMMAND
+
+if [ -z "$COMMAND" ]; then
+  usage
+  exit 1
+fi
+echo "$COMMAND"
 if [ -d $1 ]; then
   OPERATION="$COMMAND -k"
-  #d2u
-  #u2d
   
   for FILE in $1/*; do
+    #echo -n "$FILE ..."
     if [ -f "$FILE" ]; then
-      $OPERATION $CURRENT_FOLDER/$FILE
+      $OPERATION $FILE
     fi
   done
-
+echo
 
 fi
